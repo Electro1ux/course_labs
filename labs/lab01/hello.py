091ee5c7bcd979bb7f1b6b1157cbb58b2d18a3fc
Ilia <zujkoffi@yandex.ru>
2025-11-22 13:06:38 +0300

    patch2: refactor code style, resolve conflict

--- labs/lab01/hello.py
+++ labs/lab01/hello.py
@@ -1,15 +1,15 @@
 import typer
-# test comment
+
 # another test comment to conflict
+
 def main(
     name: str,
     lastname: str = typer.Option("", help="Фамилия пользователя."),
     formal: bool = typer.Option(False, "--formal", "-f", help="Использовать формальное приветствие."),
 ):
-    if formal:
-        print(f"Добрый день, {name} {lastname}!")
-    else:
-        print(f"Привет, {name}!")
+    greeting = f"Добрый день, {name} {lastname}!" if formal else f"Привет, {name}!"
+    print(greeting)
+
 
 if __name__ == "__main__":
     typer.run(main)
