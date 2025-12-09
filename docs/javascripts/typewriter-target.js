eadc80d3961df9cc3d4f146eebed98e783913cf4
Elijah S Shmakov <gshkvbmstu@gmail.com>
2025-12-09 21:53:42 +0300

    minor edit

--- docs/javascripts/typewriter-target.js
+++ docs/javascripts/typewriter-target.js
@@ -1,9 +1,9 @@
 document.addEventListener("DOMContentLoaded", function () {
-  const text = "Stay tuned ;)";
+  const text = "Sic Parvis Magna. Auxilio Divino. Stay tuned ;) The most interesting part is just ahead";
   const el = document.getElementById("typewriter-target");
 
   const typeSpeed = 100;
-  const pauseEnd  = 400;
+  const pauseEnd  = 1250;
   let i = 0;
 
   function type() {
