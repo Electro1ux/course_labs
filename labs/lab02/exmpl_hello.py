060b24007329b1186f6d19bf19dc1435d652782b
Ilia <zujkoffi@yandex.ru>
2026-01-15 08:11:44 +0300

    lab06 files

--- labs/lab02/exmpl_hello.py
+++ labs/lab02/exmpl_hello.py
@@ -1,6 +1,6 @@
+import typer
 import sys
 import time
-
 
 def colorful_print(text):
     colors = ["\033[91m", "\033[92m", "\033[93m", "\033[94m", "\033[95m"]
@@ -9,9 +9,19 @@ def colorful_print(text):
         color = colors[i % len(colors)]
         sys.stdout.write(color + char + reset)
         sys.stdout.flush()
-        time.sleep(0.2)
-    print()
+        time.sleep(delay)
+    print()  # перенос строки после текста
+
+@app.command()
+def main(
+    name: str,
+    lastname: str = typer.Option("", help="Фамилия пользователя."),
+    formal: bool = typer.Option(False, "--formal", "-f", help="Использовать формальное приветствие."),
+):
+    greeting = f"Добрый день, {name} {lastname}!" if formal else f"Привет, {name}!"
+    colorful_print(greeting)
 
 
 if __name__ == "__main__":
     colorful_print("hello appsec world")
+
